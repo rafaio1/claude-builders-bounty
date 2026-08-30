41505be67db55ea641df4a16d52c7de627cd8b94
rafaio1 <77026867+rafaio1@users.noreply.github.com>
2026-08-30 12:29:23 +0000

    feat: scaffold Immunefi oracle manipulation PoC (potential 0k-M+)

---
+++ deliverables/immunefi-oracle-poc/test/OracleManipulation.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+contract OracleManipulationTest is Test {
+    function setUp() public {
+        // TODO: Fork mainnet at specific block
+        // vm.createSelectFork("mainnet", BLOCK_NUMBER);
+    }
+    
+    function testExploit() public {
+        // TODO: Implement attack sequence
+        // 1. Setup attacker state
+        // 2. Execute manipulation
+        // 3. Assert profit > 0
+    }
+}
